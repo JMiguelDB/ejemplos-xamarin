f2cadcafb1d5818172287bd18e9fe245073af055
José Miguel <jose.miguel.db.17894@gmail.com>
2017-11-03 14:45:04 +0100

    Proyecto de ejemplo para pop-ups

---
+++ ejemplos-popup/ihealth/Pages/LoginSuccessPopupPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System.Threading.Tasks;
+using Xamarin.Forms.Xaml;
+
+namespace ihealth.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginSuccessPopupPage : PopupPage
+    {
+        public LoginSuccessPopupPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            HidePopup();
+        }
+
+        private async void HidePopup()
+        {
+            await Task.Delay(4000);
+            await PopupNavigation.RemovePageAsync(this);
+        }
+    }
+}
---
+++ ejemplos-popup/ihealth/Pages/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using System;
+using Xamarin.Forms;
+using Rg.Plugins.Popup.Pages;
+
+namespace ihealth.Pages
+{
+
+    public partial class MainPage : ContentPage
+    {
+        private LoginPopupPage _loginPopup;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _loginPopup = new LoginPopupPage();
+        }
+
+        private async void OnOpenPupup(object sender, EventArgs e)
+        {
+            //var page = new LoginPopupPage();
+            await Navigation.PushPopupAsync(_loginPopup);
+        }
+
+        private async void OnUserAnimationPupup(object sender, EventArgs e)
+        {
+            var page = new UserAnimationPage();
+            await Navigation.PushPopupAsync(page);
+        }
+
+        private async void OnOpenSystemOffsetPage(object sender, EventArgs e)
+        {
+            var page = new SystemOffsetPage();
+            await Navigation.PushPopupAsync(page);
+        }
+
+        private async void OnOpenListViewPage(object sender, EventArgs e)
+        {
+            var page = new ListViewPage();
+            await Navigation.PushPopupAsync(page);
+        }
+    }
+}
